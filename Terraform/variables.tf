5381c9f7720234221d2c8efb45f48183a6fa185f
Olivia <oliviaokakpu@gmail.com>
2025-08-20 14:14:30 +0100

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -3,7 +3,7 @@ variable "instance_name" {
 }
 
 variable "key_name" {
-  default = "keypair"                  # Names of key in aws
+  default = "terra"                  # Names of key in aws
 }
 
 
