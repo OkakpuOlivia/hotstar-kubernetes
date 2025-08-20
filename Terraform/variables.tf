6136fe18025d48cbcbf4ad28e523dd2b297cbd96
Olivia <oliviaokakpu@gmail.com>
2025-08-20 14:12:29 +0100

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -8,10 +8,10 @@ variable "key_name" {
 
 
 variable "access_key" {
-  default = "AKIA4HWJTXRTQPOETBL4"                # aws access key
+  default = "insert"                # aws access key
 }
 
 variable "secret_key" {
-  default = "SZO4NFrJBRxtesJK4REz29K9VXk4UMP3MQwp5Prc"         # aws secret key
+  default = "insert"         # aws secret key
 }
 
